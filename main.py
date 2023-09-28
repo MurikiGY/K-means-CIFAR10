06bca2fa2d52480c7e15814dc1b0353e00c11800
Muriki <mgy20@inf.ufpr.br>
2023-09-28 20:07:33 -0300

    Started project

---
+++ main.py
@@ -0,0 +1,10 @@
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import os
+import shutil
