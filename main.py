0ebad59d6a7be6bd030e829319dc9e7867486821
Muriki G. Yamanaka <64929739+MurikiGY@users.noreply.github.com>
2023-10-03 19:40:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.applications.inception_v3 import preprocess_input
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import img_to_array
-from sklearn.cluster import KMeans
-import pandas as pd
-import numpy as np
-from tqdm import tqdm
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
-import shutil
+import matplotlib.pyplot as plt
+
+from sklearn.manifold import TSNE
+from keras.datasets import cifar10
+import matplotlib.pyplot as plt
+
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+X = x_train.reshape(x_train.shape[0], -1)
+y = y_train.reshape(-1)
+
+# Apply t-SNE
+tsne = TSNE(n_components=2, perplexity=30)
+X_tsne = tsne.fit_transform(X)
+
+# Visualize the t-SNE results
+plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=y, cmap='viridis')
+plt.colorbar()
+plt.show()
+
