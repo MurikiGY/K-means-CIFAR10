e30ff59060ab2c8ee9350fccced81b3872ea329a
Wellington Almeida <aswellington200@gmail.com>
2023-10-18 06:15:07 -0300

    added cifar10 label to images

--- pixel.py
+++ pixel.py
@@ -1,6 +1,5 @@
 import os
 import imageio
-import np_utils
 import numpy as np
 from keras.datasets import cifar10
 from sklearn.cluster import KMeans
@@ -18,8 +17,9 @@ x_test = x_test.reshape(x_test.shape[0], -1)
 # Apply K-means clustering
 num_clusters = 10  # You can change this number based on the number of desired clusters
 kmeans = KMeans(n_clusters=num_clusters)
-kmeans.fit(x_train)
-cluster_labels = kmeans.predict(x_train)
+#kmeans.fit(x_train)
+#cluster_labels = kmeans.predict(x_train)
+cluster_labels = kmeans.fit_predict(x_train)
 
 # Create directories for each cluster label
 for i in range(num_clusters):
@@ -31,7 +31,7 @@ for idx, label in tqdm(enumerate(cluster_labels), total=len(cluster_labels)):
     image = x_train[idx].reshape(32, 32, 3) * 255  # Reshape the image data
     image = image.astype(np.uint8)  # Convert to unsigned integer (0 to 255)
     cluster_dir = f'cluster_{label}'
-    image_filename = f'{idx}.png'
+    image_filename = f'{idx}_{y_train[idx]}.png'
     image_path = os.path.join(cluster_dir, image_filename)
     # Save the image
     imageio.imwrite(image_path, image)
